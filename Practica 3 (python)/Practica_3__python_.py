25030cd15bf046f00513dfbea4244dab5a001526
Lizeth-P18 <146896889+Lizeth-P18@users.noreply.github.com>
2023-10-03 18:21:04 -0600

    Practica_3__ phyton

---
+++ Practica 3 (python)/Practica_3__python_.py
@@ -0,0 +1,16 @@
+#Brenda Lizeth Palasio Gutiérrez
+
+numeros=[10,20,30,40,50,60]
+print(numeros)
+print(numeros[1])
+n=[3,4,5]
+print(n)
+print(n[2])
+c=["L",'A','L',"O"]
+print(c)
+print(c[1])
+print(c[2])
+s="Mona Lisa"
+print(s)
+print(type(s))
+print(s[4])
