6b2d5fe91cf6dd83de2dd0cac4df93867d664e04
Lizeth-P18 <146896889+Lizeth-P18@users.noreply.github.com>
2023-10-03 18:20:43 -0600

    Práctica 3 (c++)

---
+++ Practica 3 (c++)/Practica 3 (c++).cpp
@@ -0,0 +1,19 @@
+//Brenda Lizeth Palasio Gutiérrez
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main() {
+	int numeros[6] = { 10,20,30,40,50,60 }, n[] = { 3,4,5 };
+	char c[] = { 'L','a','l','o' }, s[] = "Mona Lisa";
+
+	cout << numeros[1] << endl;
+	cout << n[2] << endl;
+	cout << c[0] << endl;
+	cout << c[1] << endl;
+	cout << s << endl;
+
+	system("PAUSE");
+	return 0;
+}
