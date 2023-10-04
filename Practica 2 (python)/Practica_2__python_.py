a6b74522fa0a996159f1268339c0ae8ef257d1f3
Lizeth-P18 <146896889+Lizeth-P18@users.noreply.github.com>
2023-10-03 18:09:39 -0600

    Practica_2__ phyton

---
+++ Practica 2 (python)/Practica_2__python_.py
@@ -0,0 +1,9 @@
+#Brenda Lizeth Palasio Gutiérrez
+
+NUM = 8
+nums = [0] * NUM 
+total = 0
+for i in range(NUM):
+        nums[i] = int(input("Por favor, introduzca el numero: "))
+        total += nums[i]
+        print("El total de numeros es: ", total)
