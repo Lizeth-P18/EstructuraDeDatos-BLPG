16c896724104903fa9a4f0f3a202a0103e1decc1
Lizeth-P18 <146896889+Lizeth-P18@users.noreply.github.com>
2023-10-29 20:37:24 -0600

    Practica 11 Phyton

---
+++ Practica 11 Python/Practica_11_Python.py
@@ -0,0 +1,8 @@
+#Brenda Lizeth Palasio Gutiérrez
+#Ejercicios de Recursividad e Iteración
+
+NUM = int(input("DIGITE NUMERO: "))
+for I in range(1, 13):
+    RESULT = NUM * I
+    print(f"{NUM} * {I} = {RESULT}")
+input("Pulse una tecla: ")
